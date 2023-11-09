28dc3a651dac04c0448d4cee137d6b9d2548404d
Hiroyko <edwin.haffner@heig-vd.ch>
2023-11-09 14:46:25 +0100

    continued the config class idk

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,8 @@
 package src;
 
+import java.io.*;
+import java.net.*;
+
 public class Client {
+
 }
--- src/Config.java
+++ src/Config.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 //Used this to help myself with the setup of the config file : https://stackoverflow.com/questions/16273174/how-to-read-a-configuration-file-in-java
@@ -22,13 +24,21 @@ public class Config {
         try (FileInputStream fis = new FileInputStream(fileName)) {
             prop.load(fis);
         } catch (FileNotFoundException ex) {
-            System.out.println("config file not found !");
+            System.out.println("Config file not found !");
         } catch (IOException ex) {
-
+            System.err.println(ex.getMessage());
         }
         //For the array, in the config file, we'll use "#" to delimit the emails and the messages
         String[] email = prop.get("emails").toString().split("#");
         Collections.addAll(victimsList, email);
+
+        try{
+            if(!validateAllEmail()){
+                throw new RuntimeException("One of the email in the list is invalid !");
+            }
+        }catch (RuntimeException e){
+            System.err.println(e.getMessage());
+        }
 
         String[] messages = prop.get("messages").toString().split("#");
         Collections.addAll(messagesList, messages);
@@ -36,5 +46,25 @@ public class Config {
         this.nbGroup = nbGroup;
     }
 
+    public boolean isEmailValid(String email){
+
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+    public boolean validateAllEmail(){
+
+        for(String s : victimsList) {
+            if(!isEmailValid(s)) return false;
+        }
+        return true;
+    }
 
 }
