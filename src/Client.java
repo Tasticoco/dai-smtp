4470ca9a35581fcba7402609f2394623f3e32647
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2023-11-16 11:13:12 +0100

    client start

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,27 @@
 package src;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
+
+
+    public static void main(String args[]) {
+        try (Socket socket = new Socket("localhost", 1234);
+        var in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(),
+                        StandardCharsets.UTF_8));
+        var out = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream(),
+                        StandardCharsets.UTF_8))){
+            for (int i = 0; i < 10; i++) {
+                out.write("Hello " + i + "\n");
+                out.flush();
+                System.out.println("Echo: " + in.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Client: exc.: " + e);
+        }
+    }
+
 }
