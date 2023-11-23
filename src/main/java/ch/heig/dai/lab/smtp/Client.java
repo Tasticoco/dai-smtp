006d6c75648867a4c4d923919beb2a7c1d60c14c
Neroil <edwin.haffner@heig-vd.ch>
2023-11-23 13:08:00 +0100

    doesn't work too much idk guhh

--- src/main/java/ch/heig/dai/lab/smtp/Client.java
+++ src/main/java/ch/heig/dai/lab/smtp/Client.java
@@ -10,11 +10,67 @@ public class Client {
     private static int port = 1025;
     private static String ipv4 = "localhost";
 
-    private static final String[] smtpRcvMess = {"250 HELP", "250 SMTPUTF8", "250 Accepted", "354 Start mail input; end with <CRLF>.<CRLF>", "550 No such user here","554 Transaction has failed "};
+    private static final String[] smtpRcvMess = {"220 ","250 ","354 ", "550 ","554 ","221 "};
 
     public Client(int port,String ipv4){
         Client.port = port;
         Client.ipv4 = ipv4;
+    }
+
+    public static void sendMessage(SMTPConstructor mail,BufferedWriter out,BufferedReader in) throws IOException {
+
+        String line;
+        String lineOut;
+
+        lineOut = mail.hello();
+        out.write(lineOut);
+        System.out.print(lineOut);
+
+        out.flush();
+        while(!(line = in.readLine()).contains(smtpRcvMess[0])){
+            System.out.println(line);
+        }
+        System.out.println(line);
+        lineOut = mail.from();
+        out.write(lineOut);
+        System.out.println(lineOut);
+        out.flush();
+
+        while(!(line = in.readLine()).contains(smtpRcvMess[1])){
+            System.out.println(line);
+        }
+        System.out.println(line);
+        lineOut = mail.rcptTo();
+        out.write(lineOut);
+        System.out.println(lineOut);
+        out.flush();
+        while(!(line = in.readLine()).contains(smtpRcvMess[1])){
+            System.out.println(line);
+        }
+        System.out.println(line);
+        lineOut = mail.data();
+        out.write(lineOut);
+        System.out.println(lineOut);
+        out.flush();
+        while(!(line = in.readLine()).contains(smtpRcvMess[2])){
+            System.out.println(line);
+        }
+        System.out.println(line);
+        while(!(line = in.readLine()).contains(smtpRcvMess[1])){
+            System.out.println(line);
+        }
+        System.out.println(line);
+//        System.out.println(line);
+//        lineOut = mail.quit();
+//        out.write(lineOut);
+//        System.out.println(lineOut);
+//        out.flush();
+//
+//        while(!(line = in.readLine()).contains(smtpRcvMess[5])){
+//            System.out.println(line);
+//        }
+//        System.out.println(line);
+
     }
 
     public static void main(String[] args) {
@@ -42,49 +98,14 @@ public class Client {
                     "Regards,\n" +
                     "Sender\n");
 
-            String line;
-            String lineOut;
 
-            lineOut = testMail.hello();
-            out.write(lineOut);
-            System.out.println(lineOut);
-
-            out.flush();
-            while(!(line = in.readLine()).equals(smtpRcvMess[1])){
-                System.out.println(line);
-            }
-            System.out.println(line);
-            lineOut = testMail.from();
-            out.write(lineOut);
-            System.out.println(lineOut);
-            out.flush();
-
-            while(!(line = in.readLine()).equals(smtpRcvMess[2])){
-                System.out.println(line);
-            }
-            System.out.println(line);
-            lineOut = testMail.rcptTo();
-            out.write(lineOut);
-            System.out.println(lineOut);
-            out.flush();
-            while(!(line = in.readLine()).equals(smtpRcvMess[2])){
-                System.out.println(line);
-            }
-            System.out.println(line);
-            lineOut = testMail.data();
-            out.write(lineOut);
-            System.out.println(lineOut);
-            out.flush();
-            while(!(line = in.readLine()).equals(smtpRcvMess[0])){
-                System.out.println(line);
-            }
-            System.out.println(line);
-            lineOut = testMail.quit();
-            out.write(lineOut);
-            System.out.println(lineOut);
-            out.flush();
+            sendMessage(testMail,out,in);
+            TimeUnit.MILLISECONDS.sleep(1000);
+            sendMessage(testMail,out,in);
 
 
+
+            System.out.println("Client: end");
 
 
 
