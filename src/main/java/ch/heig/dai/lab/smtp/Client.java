df8d509969a4dc8b12745662a0c7b097aa2f1305
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2023-11-23 11:59:47 +0100

    Client, SMTPConstructor

--- src/main/java/ch/heig/dai/lab/smtp/Client.java
+++ src/main/java/ch/heig/dai/lab/smtp/Client.java
@@ -9,7 +9,7 @@ public class Client {
     private static int port = 1025;
     private static String ipv4 = "localhost";
 
-    static Config config;
+    private static final String[] smtpRcvMess = {"250 HELP", "250 OK", "354 Start mail input; end with <CRLF>.<CRLF>", "550 No such user here","554 Transaction has failed "};
 
     public Client(int port,String ipv4){
         Client.port = port;
@@ -26,8 +26,41 @@ public class Client {
                     new OutputStreamWriter(socket.getOutputStream(),
                         StandardCharsets.UTF_8)))
         {
+            System.out.println("Prank program SMTP");
 
-            config = new Config(4);
+
+            SMTPConstructor testMail = new SMTPConstructor("sender@example.com","Sender",
+                    "recipient@example.com","Recipient",null,
+                    "Test Email",
+                    "Hello,\n" +
+                    "\n" +
+                    "This is a test email sent via SMTP.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "Sender\n");
+
+            String line;
+            out.write(testMail.hello());
+            while(!(line = in.readLine()).equals(smtpRcvMess[0])){
+                System.out.println(line);
+            }
+            out.write(testMail.from());
+            if((line = in.readLine()).equals(smtpRcvMess[1])){
+                System.out.println(line);
+                out.write(testMail.rcptTo());
+            }
+            while((line = in.readLine()).equals(smtpRcvMess[1])){
+                System.out.println(line);
+                out.write(testMail.rcptToCC());
+            }
+            if((line = in.readLine()).equals(smtpRcvMess[2])){
+                System.out.println(line);
+                out.write(testMail.data());
+            }
+            if((line = in.readLine()).equals(smtpRcvMess[1])){
+                System.out.println(line);
+                out.write(testMail.quit());
+            }
 
 
 
---
+++ src/main/java/ch/heig/dai/lab/smtp/SMTPConstructor.java
@@ -0,0 +1,73 @@
+package ch.heig.dai.lab.smtp;
+
+public class SMTPConstructor {
+
+    final private String mailFrom
+                  ,mailFromUsername
+                  ,mailTo
+                  ,mailToUsername
+                  ,subject
+                  ,messageText;
+
+    final private String[] carbonCopy; //CC in the mail
+
+    final public int carbonCopyLength;
+
+    public SMTPConstructor(String mailFrom,String mailFromUsername,String mailTo,String mailToUsername,String[] carbonCopy,String subject,String messageText){
+        this.mailFrom = mailFrom;
+        this.mailFromUsername = mailFromUsername;
+        this.mailTo = mailTo;
+        this.mailToUsername = mailToUsername;
+
+        if (carbonCopy != null || carbonCopy.length != 0){
+            carbonCopyLength = carbonCopy.length;
+            this.carbonCopy = new String[carbonCopy.length];
+            System.arraycopy(carbonCopy,0,this.carbonCopy,0,carbonCopy.length);
+        } else {
+            carbonCopyLength = 0;
+            this.carbonCopy = null;
+        }
+
+        this.subject = subject;
+        this.messageText = messageText;
+    }
+
+    public String data(){
+
+        String data;
+
+        data = "From: " + mailFromUsername + " <" + mailFrom + ">\r\n";
+        data += "To: " + mailToUsername + " <" + mailTo + ">\r\n";
+        data += "CC: " + carbonCopy[0] + "\r\n";
+        data += "Subject: " + subject + "\r\n";
+        data += "\r\n";
+        data += messageText + "\r\n";
+        data += ".\r\n";
+
+        return data;
+    }
+
+    public String from(){
+        return "MAIL FROM: <" + mailFrom + ">\r\n";
+    }
+
+    public String rcptTo(){
+        return "RCPT TO: <" + mailTo + ">\r\n";
+    }
+
+    public String rcptToCC(){
+        return "RCPT TO: <" + carbonCopy + ">\r\n";
+    }
+
+    public String hello(){
+        return "EHLO " + "https://www.youtube.com/watch?v=dQw4w9WgXcQ" + "\r\n";
+    }
+
+    public String quit(){
+        return "QUIT\r\n";
+    }
+
+
+
+
+}
